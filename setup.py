53b9e816d2c7d1f95246ca84b55bee190c60989a
Philipp Hagemeister <phihag@phihag.de>
2012-09-20 20:06:23 +0200

    re-add setup.py to appease mr.developer

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+version = '1.0'
+
+setup(name='adhocracy.adhocracy.hhu_theme',
+      version=version,
+      description="Adhocracy theme for https://normsetzung.cs.uni-duesseldorf.de/",
+      license='GPL',
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
